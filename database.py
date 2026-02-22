f33cd8f4360b49fd2afe8823afe03c4fc81ef8da
adamtrofimov76-collab <adamtrofimov76@gmail.com>
2026-02-23 02:36:23 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,25 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from models import Base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# меняем на asyncpg
-if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+if DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace(
         "postgresql://",
         "postgresql+asyncpg://"
     )
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, echo=False)
 
 SessionLocal = sessionmaker(
     engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)

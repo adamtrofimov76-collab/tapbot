010a2d209741c0d4dc913f5f855f54c88d46fa8f
adamtrofimov76-collab <adamtrofimov76@gmail.com>
2026-02-23 01:51:04 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не найден в переменных среды")
+
+# Railway даёт postgres:// — меняем на async
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+asyncpg://"
+    )
+
+engine = create_async_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
